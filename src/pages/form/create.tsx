e0417cebb4a974f2cb34ae7eb593d22be5736a5e
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-04-05 00:15:35 +0700

    refactor routes using javascript object

---
+++ src/pages/form/create.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const CreateFormPage = (): JSX.Element => <div>create</div>;
+
+export default CreateFormPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,69 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Route,
+  RouteComponentProps,
+  Switch
+} from 'react-router-dom';
 
 import HomePage from './pages';
 import IndividualFormPage from './pages/form/individual';
+import CreateFormPage from './pages/form/create';
+
+interface RouteType {
+  path: string;
+  exact?: boolean;
+  nested?: RouteType[];
+  component?: // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
+}
+
+const routes: RouteType[] = [
+  {
+    path: '/',
+    exact: true,
+    component: HomePage
+  },
+  {
+    path: '/form',
+    nested: [
+      {
+        path: '/create',
+        exact: true,
+        component: CreateFormPage
+      },
+      {
+        path: '/:id',
+        component: IndividualFormPage
+      }
+    ]
+  }
+];
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/form/:id" component={IndividualFormPage} />
+      {routes.map((route) =>
+        route.nested ? (
+          <Switch key={route.path}>
+            {route.nested.map((nestedRoute) => (
+              <Route
+                exact={nestedRoute.exact}
+                path={`${route.path}${nestedRoute.path}`}
+                key={`${route.path}${nestedRoute.path}`}
+                component={nestedRoute.component}
+              />
+            ))}
+          </Switch>
+        ) : (
+          <Route
+            key={route.path}
+            path={`${route.path}`}
+            component={route.component}
+            exact={route.exact}
+          />
+        )
+      )}
     </BrowserRouter>
   );
 };
