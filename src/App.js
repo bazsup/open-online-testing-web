b86aaa84ba4eb43b493152013de33caefe02235b
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2020-07-15 00:42:38 +0700

    :sparkles: Manage website route with react-router

--- src/App.js
+++ src/App.js
@@ -1,128 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import Navbar from './components/Navbar'
-import TypeSelector from './components/TypeSelector'
-import QuestionAccordion from './components/QuestionAccordion'
-import {
-  Segment,
-  Button as UnstyleButton,
-  Accordion,
-  Search,
-} from 'semantic-ui-react'
-import styled from '@emotion/styled'
-import { Empty } from 'antd'
-import { color } from './constants'
-
-const Title = styled.h2`
-  font-size: 24px;
-  display: inline-block;
-  margin: 0;
-`
-
-const Button = styled(UnstyleButton)`
-  background-color: ${color.purple} !important;
-  color: white !important;
-`
-
-const data = [
-  {
-    id: 1,
-    name: 'If you have 20 harems, what place that you must go?',
-    type: 'OBJECTIVE',
-    choices: [
-      {
-        id: 1,
-        label: 'Berlin',
-        isCorrectAnswer: false,
-      },
-      {
-        id: 2,
-        label: 'Leipzig',
-        isCorrectAnswer: false,
-      },
-      {
-        id: 3,
-        label: 'Munich',
-        isCorrectAnswer: true,
-      },
-      {
-        id: 4,
-        label: 'Zurich',
-        isCorrectAnswer: false,
-      },
-    ],
-    categories: [
-      {
-        label: 'history',
-        backgroundColor: '#2d2a4a',
-        color: '#ffffff',
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: 'Why 1+1 = 10 in your opinion?',
-    type: 'SUBJECTIVE',
-    categories: [
-      {
-        label: 'computer',
-        backgroundColor: '#2d2a4a',
-        color: '#ffffff',
-      },
-      {
-        label: 'math',
-        backgroundColor: '#000000',
-        color: '#ffffff',
-      },
-    ],
-  },
-]
+import ManagePage from './pages/manage'
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar />
-      <div className='container mt-5'>
-        <div className='d-flex justify-content-center'>
-          <TypeSelector label='จัดการคำถาม' icon='question' active />
-          <TypeSelector label='จัดการข้อสอบ' icon='exam' />
-        </div>
-        <div className='row justify-content-md-center mt-3'>
-          <div className='col-10'>
-            <Segment className='px-5 py-5'>
-              <div className='row'>
-                <div className='col-9'>
-                  <div className='d-flex justify-content-between align-items-center mb-4'>
-                    <Title>จัดการคำถาม</Title>
-                    <Button content='+ สร้างคำถาม' />
-                  </div>
-                  {data.length === 0 ? (
-                    <Segment>
-                      <Empty />
-                    </Segment>
-                  ) : (
-                    <React.Fragment>
-                      <Search className='mb-3' />
-                      <Accordion fluid styled>
-                        {data.map(
-                          ({ name, type, choices, categories }, index) => (
-                            <QuestionAccordion
-                              name={name}
-                              type={type}
-                              choices={choices}
-                              categories={categories}
-                              index={index}
-                            />
-                          )
-                        )}
-                      </Accordion>
-                    </React.Fragment>
-                  )}
-                </div>
-              </div>
-            </Segment>
-          </div>
-        </div>
-      </div>
+      <Router>
+        <Navbar />
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path='/manage'>
+            <ManagePage />
+          </Route>
+          <Route path='/'>ดีงัฟฟ</Route>
+        </Switch>
+      </Router>
     </React.Fragment>
   )
 }
