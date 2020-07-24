55fe1a903cb81252bfd26e7cd2158d0d8ce8df76
NaomiLin <wdrdres3qew5ts21@gmail.com>
2020-07-24 17:17:42 +0700

    [ADD] Jenkins Masterfile

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://b37f3139db5b.ngrok.io/',
+  // baseURL: 'http://b37f3139db5b.ngrok.io/',
+  // baseURL: 'https://api.devopsculture.tk/depa/'
+  baseURL: 'http://depa-backend-service/',
 })
 
 export default api
