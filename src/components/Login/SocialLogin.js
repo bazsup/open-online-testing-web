cffe256926d9ae0f724346750ea26f9be854ff06
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-08-19 22:38:46 +0700

    add facebook and google logo in social login

--- src/components/Login/SocialLogin.js
+++ src/components/Login/SocialLogin.js
@@ -1,14 +1,30 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Image } from 'semantic-ui-react'
 
 const SocialLogin = () => {
   return (
     <div>
       <div className="my-2">
-        <Button fluid basic>Google</Button>
+        <Button fluid basic>
+          <Image
+            avatar
+            spaced="right"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png"
+            alt="google logo"
+          />
+          Continue with Google
+        </Button>
       </div>
       <div className="my-2">
-        <Button fluid basic>Facebook</Button>
+        <Button fluid basic>
+          <Image
+            avatar
+            spaced="right"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png"
+            alt="facebook logo"
+          />
+          Continue with Facebook
+        </Button>
       </div>
     </div>
   )
