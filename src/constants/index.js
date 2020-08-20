aadb751388cf9a5c95ab25d2fc909a21c1b7199e
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-08-20 22:42:46 +0700

    [UPDATE] language constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,13 +10,33 @@ export const JWT_TOKEN = 'jwtToken'
 
 export const lang = {
   th: {
+    // common
+    pleaseTrygain: 'เกิดข้อผิดพลาด โปรดลองใหม่อีกครั้ง',
+
+    // login
     loginSuccess: 'เข้าสู่ระบบสำเร็จ',
     loginFail: 'เข้าสู่ระบบไม่สำเร็จ โปรดลองใหม่อีกครั้ง',
     login: 'เข้าสู่ระบบ',
     email: 'อีเมล',
     password: 'รหัสผ่าน',
-    doesNotHaveAnAccount: 'ยังไม่มีบัญชี ?',
-    register: 'สมัครสมาชิก',
-    or: 'หรือ'
+    doesNotHaveAnAccount: 'ยังไม่มีบัญชี?',
+    or: 'หรือ',
+    clickToRegister: 'คลิกเพื่อลงทะเบียน',
+    
+    // register page
+    register: 'ลงทะเบียน',
+    fullname: 'ชื่อ-นามสกุล',
+    registerTitle: 'ลงทะเบียนฟรี!',
+    registerSuccess: `ลงทะเบียนสำเร็จ`,
+    alreadyHaveAccount: 'มีบัญชีอยู่แล้ว?',
+    passwordHint: 'รหัสผ่านต้องประกอบด้วย ตัวอักษรทั้งพิมพ์เล็ก พิมพ์ใหญ่ และตัวเลขผสมกัน ความยาวไม่น้อยกว่า 8 ตัวอักษร',
+    passwordHintCheckList: ['ตัวอักษร (a-z, A-Z)', 'ตัวเลข (0-9)'],
+
+    // header
+    welcome: 'ยินดีต้อนรับ',
+    logout: 'ออกจากระบบ',
+
+    // error
+    invalidEmailOrPassword: '⚠️ อีเมลหรือรหัสผ่านไม่ถูกต้อง'
   }
 }
