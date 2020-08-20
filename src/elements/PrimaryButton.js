23606e2d841b2dc1dcb2cbad1e58873c04637814
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-08-20 22:44:03 +0700

    [UPDATE] remove prop because it can inject using props

--- src/elements/PrimaryButton.js
+++ src/elements/PrimaryButton.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
 
-export const PrimaryButton = ({ className, children, ...props }) => (
-  <Button color="orange" className={className} {...props}>
+export const PrimaryButton = ({ children, ...props }) => (
+  <Button color="orange" {...props}>
     {children}
   </Button>
 )
