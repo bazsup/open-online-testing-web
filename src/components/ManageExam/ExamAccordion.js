084a3dc05a1d718720f4ff1a342ee8f4a40a5235
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2020-09-26 23:05:34 +0700

    [UPDATE] Able to copy exam url room

--- src/components/ManageExam/ExamAccordion.js
+++ src/components/ManageExam/ExamAccordion.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { Accordion, Button, Icon } from 'semantic-ui-react'
+import { Accordion, Button, Icon, Popup } from 'semantic-ui-react'
 import { useCallback } from 'react'
 import { CategoryList } from '../Manage/CategoryList'
+import { toast } from '../../libs/toast'
 
 const useToggle = (initial = false) => {
   const [active, setActive] = useState(initial)
@@ -12,29 +13,47 @@ const useToggle = (initial = false) => {
   return [active, toggleActive]
 }
 
-export default ({ name, description, categories }) => {
+export default ({ id, name, description, categories }) => {
   const [active, toggleActive] = useToggle(false)
+  const examLink = `${process.env.REACT_APP_BASE_APP_URL}/exam/${id}`
+
+  const onCopyURLClick = () => {
+    navigator.clipboard.writeText(examLink)
+    toast.success(`Copied exam link: ${examLink}`)
+  }
+
   return (
     <React.Fragment>
       <Accordion.Title
         active={active}
-        className='d-flex justify-content-between align-items-start'
+        className="d-flex justify-content-between align-items-start"
         onClick={toggleActive}
       >
         <div>
           <div>
             <strong>{name}</strong>
           </div>
-          <div>
-            {`รายละเอียด: ${description}`}
-          </div>
+          <div>{`รายละเอียด: ${description}`}</div>
 
           {categories.length > 0 && <CategoryList categories={categories} />}
         </div>
         <div>
-          <Button icon disabled>
-            <Icon name='edit' />
-          </Button>
+          <Button.Group>
+            <Popup
+              position="top right"
+              trigger={
+                <Button icon onClick={onCopyURLClick}>
+                  <Icon name="copy" />
+                </Button>
+              }
+              content={examLink}
+              inverted
+            />
+
+            <Button icon disabled>
+              <Icon name="edit" />
+            </Button>
+          </Button.Group>
         </div>
       </Accordion.Title>
     </React.Fragment>
