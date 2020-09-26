59ca36c98dec03874ef059c0c1a19d6ba67dda27
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2020-09-26 19:20:35 +0700

    :fire: Remove unuse import

--- src/index.js
+++ src/index.js
@@ -10,31 +10,29 @@ import 'antd/dist/antd.css'
 import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 import App from './App'
-import jwtDecode from 'jwt-decode'
-import api from './api/instance'
 
 ReactDOM.render(
   <React.StrictMode>
     <Helmet>
       <link
-        rel='stylesheet'
-        href='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css'
-        integrity='sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I'
-        crossorigin='anonymous'
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
+        integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I"
+        crossorigin="anonymous"
       />
       <link
-        rel='stylesheet'
-        href='https://i.icomoon.io/public/temp/7baef22a04/UntitledProject/style.css'
+        rel="stylesheet"
+        href="https://i.icomoon.io/public/temp/7baef22a04/UntitledProject/style.css"
       ></link>
       <script
-        src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'
-        integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo'
-        crossorigin='anonymous'
+        src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"
       ></script>
       <script
-        src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'
-        integrity='sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/'
-        crossorigin='anonymous'
+        src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
+        integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/"
+        crossorigin="anonymous"
       ></script>
     </Helmet>
     <App />
